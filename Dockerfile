110239b703376f9be32135bb2f6a379741313ce1
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2017-12-20 00:33:04 -0500

    実行可能な状態に落としこむ

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM tiangolo/uwsgi-nginx-flask:python3.6
-
-RUN yum install -y mysql mysql-server
-
-RUN echo "NETWORKING=yes" >> /etc/sysconfig/network
-ADD ./setup.sql
-RUN /usr/bin/mysql_safe & sleep 10s && cat setup.sql | mysql
-
-CMD ["/usr/bin/mysqld_safe"]
--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 config = {
     "user": "root",
-    "password": "",
-    "host": "localhost",
+    "password": "honya_morake",
+    "host": "db",
     "db": "whiteboard"
 }
 
@@ -16,7 +16,6 @@ con = mysql.connect(
         db = config["db"]
 )
 
-
 @app.route("/")
 def hello():
     #TODO:部屋の名前が欲しい.そのあとindex.htmlにその情報をリストで表示
@@ -26,23 +25,22 @@ def hello():
     rows = cur.fetchall()
 
     with open('./static/index.html', 'r') as file:
-        return file.read() + "huehue"
+        return file.read()
 
 ##ボタン的なの押したら部屋作れるようにしたい
-@app.route("/room_name", methos=["POST"])
-@content_type('application/json')
+@app.route("/room_name", methods=["POST"])
 def create_room():
     if not request.headers.get("Content-Type") == 'application/json':
         error_message = {
             'error':'not supported Content-Type'
         }
         return make_response(jsonify(error_message), 400)
-    
+
     cur = con.cursor()
     sql = "insert into ********"
     cur.execute(sql)
     return make_response(jsonify({'result': True}))
-    
+
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, port=80)
